35cb437eaa31fa71066d9bccbb3336de514dfd77
Dalia Gomez <daivogg@gmail.com>
2024-04-24 12:28:18 -0600

    Deleted the test versions, modified the README file and made corrections in the index.html, blog.html and script.js files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # DIGG-Module4-Challenge-WebAPIs-PersonalBlog
 Dalia Gomez - Module 4 Challenge: Web APIs, Personal Blog
+
+## Description
+The purpose of this challenge was to create a personal blog for a marketing student, described as a "two-page website where users will input and view blog posts", to showcase thoughts and experiences. to view and manage employee payroll data. The "DIGG-Module4-Challenge-WebAPIs-PersonalBlog" repository includes two CSS files, two HTML files, two JavaScript files, a .gitignore file, a license file and this README file.
+
+## Links
+- URL of the GitHub repository that contains the code: https://github.com/dali-gogo/DIGG-Module4-Challenge-WebAPIs-PersonalBlog/
+- URL of the deployed application: dali-gogo.github.io/DIGG-Module4-Challenge-WebAPIs-PersonalBlog 
+
+## Usage
+After clicking the "Submit your idea" button, the user will be taken to a second page (blog.html), where he/she will be able to see the blog posts that have been created. The website will also have a light/dark mode option, to improve user experience.
+
+## Credits
+Modules 4 and 5 from Bootcamp TDM-VIRT-FSF-PT-03-2024-U-LOLC-MTTH were used as a reference to structure the code used in this repository. 
+
+## License
+MIT License used for the corresponding repository.                                                                                                                                                                                                                                                         
+
+## Screenshots
+![Screenshot of the website deployed.]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,138 +1,26 @@
-const newEntriesEl = $('#newEntries-form');
-const usernameInputEl = $('#username-input');
-const entryInputEl = $('#entry-input');
-const newEntriesDisplayEl = $('#newEntries-form-display');
-
-const allEntriesData = function (username, entry) {
-  const cardColumnEl = $('<div>');
-  cardColumnEl.addClass('col-12 col-sm-4 col-md-3');
-
-  const cardEl = $('<div>');
-  // add a class of .custom-card
-  cardEl.addClass('card h-100');
-  cardEl.appendTo(cardColumnEl);
-
-  // add a class of .custom-card-header
-  const cardUsername = $('<h5>').addClass('card-header custom-card-header').text(username);
-  cardUsername.appendTo(cardEl);
-
-  const cardBodyEl = $('<div>');
-  cardBodyEl.addClass('card-body');
-  cardBodyEl.appendTo(cardEl);
-
-  const cardEntry = $('<p>').addClass('card-text').text(entry);
-  cardEntry.appendTo(cardBodyEl);
-
-  newEntriesDisplayEl.append(cardColumnEl);
-};
-
-const handleFormSubmit = function (event) {
-  event.preventDefault();
-
-  const usernameInput = usernameInputEl.val();
-  const entryInput = entryInputEl.val();
-
-  if (!usernameInput || !entryInput) {
-    console.log('You need to fill out the form!');
-    return;
-  }
-
-  printallEntriesData(usernameInput, entryInput);
-
-  // reset form
-  usernameInputEl.val('');
-  entryInputEl.val('');
-};
-
-newEntriesEl.on('submit', handleFormSubmit);
-
 const todoInput = document.querySelector('#todo-text');
-const todoForm = document.querySelector('#todo-form');
-const todoList = document.querySelector('#todo-list');
-const todoCountSpan = document.querySelector('#todo-count');
+const titleInput = document.querySelector('#title-text');
+const entryInput = document.querySelector('#entry-text');
+const buttonSubmit = document.querySelector('#btn-submit');
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-let todos = [];
-
-// The following function renders items in a todo list as <li> elements
-function renderBlogPosts() {
-  // Clear todoList element and update todoCountSpan
-  todoList.innerHTML = '';
-  todoCountSpan.textContent = todos.length;
-
-  // Render a new li for each todo
-  for (let i = 0; i < todos.length; i++) {
-    const todo = todos[i];
-
-    const li = document.createElement('li');
-    li.textContent = todo;
-    li.setAttribute('data-index', i);
-
-    const button = document.createElement('button');
-    button.textContent = 'Complete ✔️';
-
-    li.appendChild(button);
-    todoList.appendChild(li);
-  }
-}
-
-// This function is being called below and will run when the page loads.
-function init() {
-  // Get stored todos from localStorage
-  const storedTodos = JSON.parse(localStorage.getItem('todos'));
-
-  // If todos were retrieved from localStorage, update the todos array to it
-  if (storedTodos !== null) {
-    todos = storedTodos;
-  }
-
-  // This is a helper function that will render todos to the DOM
-  renderBlogPosts();
-}
-
-function storeBlogPosts() {
-  // Stringify and set key in localStorage to todos array
+function storeTodos() {
   localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-localStorage.setItem('blogPosts', JSON.stringify({title:"title"}))
-// Add submit event to form
-todoForm.addEventListener('submit', function (event) {
-  event.preventDefault();
+buttonSubmit.addEventListener('click', function (event) {
 
   const todoText = todoInput.value.trim();
+  const titleText = titleInput.value.trim();
 
-  // Return from function early if submitted todoText is blank
   if (todoText === '') {
     return;
   }
 
-  // Add new todoText to todos array, clear the input
-  todos.push(todoText);
-  todoInput.value = '';
-
-  // Store updated todos in localStorage, re-render the list
-  storeBlogPosts();
-  renderBlogPosts();
+  todos.push();
+  storeTodos();
+  location.href = "blog.html";
 });
-
-// Add click event to todoList element
-todoList.addEventListener('click', function (event) {
-  const element = event.target;
-
-  // Checks if element is a button
-  if (element.matches('button') === true) {
-    // Get its data-index value and remove the todo element from the list
-    const index = element.parentElement.getAttribute('data-index');
-    todos.splice(index, 1);
-
-    // Store updated todos in localStorage, re-render the list
-    storeBlogPosts();
-    renderBlogPosts();
-  }
-});
-
-// Calls init to retrieve data and render it to the page on load
-init();
 
 const themeSwitcher = document.querySelector('#theme-switcher');
 const container = document.querySelector('.container');
